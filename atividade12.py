f63aed8f5da8dd66834024078fc821a06f6ecd7e
LainWeire <lucasfrota3000@gmail.com>
2024-09-11 14:50:32 -0300

    uff notas

---
+++ atividade12.py
@@ -0,0 +1,7 @@
+# Crie um programa que receba a nota de um aluno e informe se ele foi aprovado ou reprovado. Considere que a média para aprovação é 7.
+no = float(input("Nota do aluno: "))
+if (no >= 7):
+    print ("Aprovado")
+
+if (no < 7):
+    print ("Reprovado")
